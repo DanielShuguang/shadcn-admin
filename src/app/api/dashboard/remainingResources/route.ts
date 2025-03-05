52482740fc9e063292e3b616f26df49a73d8f225
胡曙光 <o_hushuguang@shizhuang-inc.com>
2025-03-05 10:43:54 +0800

    refactor(app): 优化 remainingResources 接口
    - 重构 GET 函数，引入 NextRequest 类型
    - 使用 req.signal.aborted 替代自定义 isAborted 标志
    - 优化错误处理和日志信息
    - 添加 finally 块确保定时器被正确清理

--- src/app/api/dashboard/remainingResources/route.ts
+++ src/app/api/dashboard/remainingResources/route.ts
@@ -1,25 +1,24 @@
 import { mockRemainingResource } from './mock'
+import { NextRequest } from 'next/server'
 
-export async function GET() {
+export async function GET(req: NextRequest) {
   const stream = new TransformStream()
   const writer = stream.writable.getWriter()
   const encoder = new TextEncoder()
 
-  let isAborted = false
   // 定义发送消息的函数
   const sender = async () => {
     try {
       const percent = await mockRemainingResource()
       await writer.write(encoder.encode(`data: ${percent}\n\n`))
     } catch (error) {
-      isAborted = true
-      console.error('Error while sending data:', error)
+      console.error('发送消息时出错:', error)
     }
   }
 
   // 设置定时器并保存其 ID
   const intervalId = setInterval(() => {
-    if (isAborted) {
+    if (req.signal.aborted) {
       clearInterval(intervalId) // 清理定时器
     } else {
       sender()
@@ -32,11 +31,13 @@ export async function GET() {
   // 监听流的关闭事件
   writer.closed
     .then(() => {
-      console.log('Client disconnected, cleaning up...')
-      clearInterval(intervalId) // 清理定时器
+      console.log('客户端已关闭连接, 停止发送消息...')
     })
     .catch(err => {
-      console.error('Writer closed with an error:', err)
+      console.error('关闭流时发生错误:', err)
+    })
+    .finally(() => {
+      clearInterval(intervalId) // 清理定时器
     })
 
   return new Response(stream.readable, {
